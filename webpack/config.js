109cb2b9c1955d3c40ced03355a7ea6741025b27
Kauan Costa <kauan@soulloop.com>
2025-06-08 12:49:14 -0300

    Add devServer configuration to webpack for improved development experience

--- webpack/config.js
+++ webpack/config.js
@@ -54,4 +54,14 @@ module.exports = {
       patterns: [{ from: "public", to: "." }],
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "../dist"),
+    },
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
 };
